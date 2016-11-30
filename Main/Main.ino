10790418ca4a9cb8734d31cacee619d68fa88f4a
SummonedEgar <aldast10@yahoo.it>
2016-11-30 20:01:19 +0100

    Main

--- Main/Main.ino
+++ Main/Main.ino
@@ -3,8 +3,9 @@
 #include <Wire.h>
 #include <math.h>
 
-#define A 0.301
 #define L0 60
+#define E 50 //min diff actuation
+
 //VL6180
 #define PIN_0 2
 #define N_Sensor 5
@@ -22,6 +23,7 @@
 #define DATA_PIN    9 //led strip
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
+#define LPF NUM_LEDS/N_Sensor
 #define FPS         100
 
 //Typedef
@@ -49,13 +51,17 @@ VL6180x sensor[N_Sensor] = {
 
 uint8_t New_VL6180[N_Sensor]={VL6180_0,VL6180_1,VL6180_2,VL6180_3,VL6180_4};
 
-data Main; 
+data Main;
+data Temp; 
+unsigned long t_running=0;
+uint8_t state[N_Sensor]={0};
+uint8_t skip_check=0;
 
 //WS2182
 CRGB leds[NUM_LEDS];
 
 //Functions
-void update_strip ( int led_num) {
+void update_led ( int led_num) {
     
   leds[led_num].setHSV(Main.Hue[led_num],Main.Sat[led_num],Main.Val[led_num]); 
   FastLED.show();
@@ -66,14 +72,33 @@ void calculate_val ( int n_sensor) { //n_sensor from 0 to 4
   
   uint8_t luminosity;
   
-  luminosity = (double)((log(Main.lux[n_sensor]/ 20971+1)/A-1)*(255-L0))+L0;
-
+  luminosity = (Main.lux[n_sensor]/10000)*(255-L0)+L0;
+  
   for (int i=0; i<(NUM_LEDS/N_Sensor); i++) {
 
     Main.Val[(i+1)*(n_sensor+1)]=luminosity;
       
   }
    
+}
+
+void update_Temp(int i) {
+   
+  Temp.lux[i]=Main.lux[i];
+  Temp.mm[i]=Main.mm[i];
+
+}
+
+void get_data (int i) {
+
+  Main.lux[i]=sensor[i].getAmbientLight(GAIN_1);
+  Main.mm[i]=sensor[i].getDistance();
+
+}
+
+int mm_diff(int i) {
+
+  return(Temp.mm[i]-Main.mm[i]);
 }
 
 void setup() { 
@@ -105,41 +130,75 @@ void setup() {
     sensor[i].VL6180xDefautSettings();
     delay(100);
     sensor[i].changeAddress(VL6180,New_VL6180[i]);
-    
+    get_data(i);
+    update_Temp(i);
   }
 
   //WS2182 Initialization
   
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-      
+
+  //Initialization
+  t_running=millis();    //t=0
+  
+
+   
 }
 
 void loop() {
 
-  short H;
-  
-  for(int i=0 ; i<N_Sensor; i++) {
-    
-    Main.lux[i]=sensor[i].getAmbientLight(GAIN_1);
-    
-    Serial.println("Sensore Numero");
-    Serial.println(i);
-    Serial.println(sensor[i].getAmbientLight(GAIN_1));
-    
-    Main.mm[i]=sensor[i].getDistance();
-    
-    calculate_val(i);
-       
-  }
+  byte check;
 
-  for(int i=0; i<NUM_LEDS; i++) {
+/////////////////IO BEGIN//////////////
+  if(millis()-t_running>300) { //Scan every 300 ms
+    
+    t_running=millis();
+    
+    for(int i=0 ; i<N_Sensor; i++) { //Get data /// determine state
+
+     update_Temp(i);
+     get_data(i);  
+/////////////////IO END///////////////    
+    
+     if(mm_diff(i)>E) {
+      state[i]=1;
+    }
+    }
+  }   
+    
+    if(skip_check!=1){
+     
+    for(int i=0 ; i<N_Sensor; i++) {
+      if (state[i]==1) { 
+
+        update_Temp(i);
+        check=1;
+        t_running=millis();
+        
+        while(check==1){
+
+          get_data(i);
+          if(mm_diff(i)>E) {
+            state[i]=0;
+          }
+          delay(250);
+          if(state[i]==0) {
+            check=0;
+          } 
+          if(millis()-t_running>1000) {
+            check=2;
+          }
+        }
+        if(check==2){
+        for(int j=0 ; j<LPF; j++){
+          Main.Val[(i*LPF+j)]=0;
+          update_led(i*LPF+j);        
+        }
+      }
+    }
+    }
   
-    Main.Hue[i]=H;
-    
-    H=H+3;
-    
-    update_strip(i);
-    
+
   }
 
   delay(1000/FPS);
